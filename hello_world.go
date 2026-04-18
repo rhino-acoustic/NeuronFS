a516a240772b93a15244f12213fe0e25ce498282
rhino-acoustic <80452162+rhino-acoustic@users.noreply.github.com>
2026-04-18 18:51:32 +0900

    [session-end] auto-save

---
+++ hello_world.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// HelloWorld returns a hello world string.
+func HelloWorld() string {
+	return "Hello, World!"
+}
+
+func main() {
+	fmt.Println(HelloWorld())
+}
---
+++ hello_world_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHelloWorld(t *testing.T) {
+	expected := "Hello, World!"
+	actual := HelloWorld()
+
+	if actual != expected {
+		t.Errorf("expected %q, but got %q", expected, actual)
+	}
+}
